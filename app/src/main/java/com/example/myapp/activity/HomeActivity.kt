b9de4d29e35caead4e78d1e3b7b1d29c5c04db8c
Shivam Shrivastava <43011013+SS7186805@users.noreply.github.com>
2023-06-20 18:27:19 +0530

    nav

--- app/src/main/java/com/example/myapp/activity/HomeActivity.kt
+++ app/src/main/java/com/example/myapp/activity/HomeActivity.kt
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.myapp.R
+import com.example.myapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     lateinit var navController: NavController
+    lateinit var binding:ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         navController = findNavController(R.id.fragmentMain)
+        NavigationUI.setupWithNavController(binding.bottomNav, navController)
 
-        navController.navigate(R.id.homeFragment)
     }
 }
